4229c1be28cb0ffa9286877453a45549a34b57b0
abdulkader33447 <abdulkader33447@gmail.com>
2025-08-28 11:52:50 +0600

    impliment google  login

--- src/app/login/components/SocialLogin.jsx
+++ src/app/login/components/SocialLogin.jsx
@@ -1,28 +1,37 @@
 "use client";
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useEffect } from "react";
 import toast from "react-hot-toast";
 import { FaGithub } from "react-icons/fa6";
 import { FcGoogle } from "react-icons/fc";
 
 const SocialLogin = () => {
   const router = useRouter();
+  const session = useSession();
 
-  const handleSocialLogin = async (providerName) => {
-    console.log("Social Login component", providerName);
-    const result = await signIn(providerName, {
-      redirect: false,
-      callbackUrl: "/",
-    });
-
-    if (result?.error) {
-      toast.error("Something went wrong");
-    } else {
-      router.push(result.url || "/");
-      toast.success(`Logged in successfully with ${providerName}`);
-    }
+  const handleSocialLogin = (providerName) => {
+    
+    signIn(providerName);
+    // console.log("Social Login component", providerName);
+    // const result = await signIn(providerName, {
+    //   redirect: false,
+    //   callbackUrl: "/",
+    // });
+    // if (result?.error) {
+    //   toast.error("Something went wrong");
+    // } else {
+    //   router.push(result.url || "/");
+    //   toast.success(`Logged in successfully with ${providerName}`);
+    // }
   };
+
+  useEffect(() => {
+    if (session?.status ==="authenticated") {
+      toast.success("Successfully logged in");
+      router.push("/");
+    }
+  }, [session?.status]);
 
   return (
     <div className="flex items-center justify-center gap-3">
